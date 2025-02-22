a065bd96f8d08f78bffac584e60cb53850f55a50
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-02-22 09:06:29 +0500

    title page experiment

---
+++ pages/title.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Streamlit App",  # Title in the browser tab
+    page_icon="🚀",  # Emoji or an image file (favicon)
+)
+
+st.title("Welcome to My Streamlit App! 🚀")
