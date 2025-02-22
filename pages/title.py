8c57b968f02ad673f2250362be6995dd1721afe2
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-02-22 09:30:02 +0500

    Silenced title.py

--- pages/title.py
+++ pages/title.py
@@ -1,8 +1,8 @@
-import streamlit as st
+# import streamlit as st
 
-st.set_page_config(
-    page_title="My Streamlit App",  # Title in the browser tab
-    page_icon="🚀",  # Emoji or an image file (favicon)
-)
+# st.set_page_config(
+#     page_title="My Streamlit App",  # Title in the browser tab
+#     page_icon="🚀",  # Emoji or an image file (favicon)
+# )
 
-st.title("Welcome to My Streamlit App! 🚀")
+# st.title("Welcome to My Streamlit App! 🚀")
